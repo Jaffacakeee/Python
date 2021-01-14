de011ad5aeead72970bd6e2fd1631244b431d176
Jaffacakeee <benlloyd95@outlook.com>
2021-01-14 21:23:20 +0000

    Started the 100 days of code course

---
+++ 100 Days of Code/Day 1- Variables/bandname.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in?\n ")
+pet = input("What is the name of your pet?\n ")
+
+print("Your band name is", city + " " + pet)
